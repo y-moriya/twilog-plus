c53a384aebb563b9e407a792e1f08e37420a3545
Yu MORIYA <euro21st@gmail.com>
2023-03-26 21:54:12 +0900

    Format main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import './style.css'
+import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.addEventListener('DOMContentLoaded', () => {
-  const keywords = document.getElementById('keywords') as HTMLTextAreaElement;
-  const saveButton = document.getElementById('saveButton') as HTMLButtonElement;
+document.addEventListener("DOMContentLoaded", () => {
+  const keywords = document.getElementById("keywords") as HTMLTextAreaElement;
+  const saveButton = document.getElementById("saveButton") as HTMLButtonElement;
 
   // Retrieve and display the saved text
-  chrome.storage.sync.get(['keywords'], (result) => {
-    keywords.value = result.keywords || '';
+  chrome.storage.sync.get(["keywords"], (result) => {
+    keywords.value = result.keywords || "";
   });
 
   // Save the input text
-  saveButton.addEventListener('click', () => {
+  saveButton.addEventListener("click", () => {
     chrome.storage.sync.set({ keywords: keywords.value }, () => {
-      console.log('Input text saved.');
+      console.log("Input text saved.");
       window.close();
     });
   });
@@ -24,4 +24,4 @@ app.innerHTML = `
   <h1>Ignore keywords</h1>
   <textarea id="keywords" rows="10" cols="30"></textarea>
   <button id="saveButton">Save</button>
-`
+`;
